b1ed0c5882df749552d63867a48fcd1f2e680c95
stumpf-alexandre <als.stumpf@gmail.com>
2024-11-25 01:10:07 -0300

    adicionando audio de fundo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -208,6 +208,9 @@ function init() {
 
     drawCards(5, state.playerSides.player);
     drawCards(5, state.playerSides.computer);
+
+    const bgm = document.getElementById("bgm");
+    bgm.play();
 }
 
 init();
